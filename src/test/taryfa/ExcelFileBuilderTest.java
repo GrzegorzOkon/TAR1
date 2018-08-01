9e0c9c185c6bb4e5949b127f469fd1395c269672
GrzegorzOkon <grzegorzokon@gmail.com>
2018-08-01 14:57:27 +0200

    Dodano plik z testem

---
+++ src/test/taryfa/ExcelFileBuilderTest.java
@@ -0,0 +1,12 @@
+package taryfa;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class ExcelFileBuilderTest {
+    @Test
+    public void nothing() {
+        assertTrue(true);
+    }
+}
